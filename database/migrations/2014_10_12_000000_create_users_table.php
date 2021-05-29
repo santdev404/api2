f42bd489f53fc858f348069579da54dfcea5101b
Santdev404 <santdev404@gmail.com>
2021-05-28 20:10:39 -0500

    migration & models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
+            $table->string('role');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->text('description');
             $table->string('password');
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
